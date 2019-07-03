06fc8100292b3b3eb0b19b8edfabe6550c00a6aa
Choonster TheMage <Choonster.2010@gmail.com>
2019-07-03 22:15:46 +1000

    Print error message when UnitClass returns nil

--- core.lua
+++ core.lua
@@ -7,7 +7,11 @@ hooksecurefunc("UnitFramePortrait_Update", function(self)
 	if self.portrait then
 		if UnitIsPlayer(self.unit) then
 			local _, class = UnitClass(self.unit)
-			self.portrait:SetTexture(TEXTURE_NAME:format(class))
+			if class then
+				self.portrait:SetTexture(TEXTURE_NAME:format(class))
+			else
+				print(("EnhancedClassIconPortraits Error: UnitClass returned nil for unit %q"):format(self.unit))
+			end
 		end
 	end
 end)
